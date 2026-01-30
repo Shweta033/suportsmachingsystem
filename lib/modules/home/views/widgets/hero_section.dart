e8a081bf104035075d223c52f9f5c40c1b21c73f
apple <sharmashweta08995@gmail.com>
2026-01-30 13:05:28 +0530

    Refactor footer and navbar components; add HeroStats widget with statistics display

--- lib/modules/home/views/widgets/hero_section.dart
+++ lib/modules/home/views/widgets/hero_section.dart
@@ -36,3 +36,66 @@ class HeroSection extends StatelessWidget {
     );
   }
 }
+
+class HeroStats extends StatelessWidget {
+  const HeroStats({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isMobile = Responsive.isMobile(context);
+
+    return Container(
+      width: double.infinity,
+      color: AppColors.buttonPrimary, // light cream bg
+      padding: EdgeInsets.symmetric(
+        horizontal: isMobile ? 20 : 80,
+        vertical: isMobile ? 40 : 56,
+      ),
+      child: isMobile
+          ? Column(children: _stats())
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: _stats(),
+            ),
+    );
+  }
+
+  List<Widget> _stats() {
+    return const [
+      _StatItem(value: '359.000.000', label: 'Company Registered'),
+      _StatItem(value: '15.000', label: 'SMS Colours'),
+      _StatItem(value: 'ISO 12647', label: 'Based Colour'),
+    ];
+  }
+}
+
+class _StatItem extends StatelessWidget {
+  final String value;
+  final String label;
+
+  const _StatItem({required this.value, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w700,
+            color: AppColors.background,
+            letterSpacing: -0.5,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          label,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 14, color: AppColors.textLight),
+        ),
+      ],
+    );
+  }
+}
